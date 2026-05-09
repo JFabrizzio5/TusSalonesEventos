3b0e05e44f96234a39722f4eb42d87a216ff858e
cesar jan karlo <leossuarez103@gmail.com>
2026-05-09 16:02:16 -0600

    se creo apiFilter

--- Backend/app/Filters/ApiFilter.php
+++ Backend/app/Filters/ApiFilter.php
@@ -5,5 +5,24 @@ use Illuminate\Http\Request;
 
 
 class ApiFilter{
+    // --- Filtro Base para API: Define parámetros seguros, mapeos de columnas y operadores permitidos ---
+    protected $safeParms = [];
+    protected $columnMap = [];
+    protected $operatorMap = [];
 
+    public function transform(Request $request){
+        $eloQuery = [];
+        foreach ($this->safeParms as $parm => $operators) {
+            $query = $request->query($parm);
+            if (!isset($query)) continue;
+
+            $column = $this->columnMap[$parm] ?? $parm;
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+        return $eloQuery;
+    }
 }
