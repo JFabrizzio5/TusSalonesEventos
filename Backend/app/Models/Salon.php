a3f2c442611d1fd571c1056e2e01249d7ac1a488
cesar jan karlo <leossuarez103@gmail.com>
2026-05-08 20:14:49 -0600

    se agrego protected

--- Backend/app/Models/Salon.php
+++ Backend/app/Models/Salon.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Salon extends Model
 {
-    //
+    //Protecion contra asignacion masiva
+    protected $table = 'salons';
+
+    protected $fillable = [
+        'name',
+        'capacity',
+        'app_id',
+        'userauth_id'
+    ];
 }
