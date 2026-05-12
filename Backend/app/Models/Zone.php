9c257b38e8e321105f2a16500d84eb684430f1f9
cesar jan karlo <leossuarez103@gmail.com>
2026-05-12 10:17:50 -0600

    se hizo Protected  de los campos

--- Backend/app/Models/Zone.php
+++ Backend/app/Models/Zone.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Zone extends Model
 {
-    //
+    //Protected $fillable 
+    protected $fillable = [
+        'salon_id',
+        'name'
+    ];
+
+    //Relacion muchos a uno(salones-zonas)
+    public function salon()
+    {
+        $this -> belongsTo(Salon::class);
+    }
 }
