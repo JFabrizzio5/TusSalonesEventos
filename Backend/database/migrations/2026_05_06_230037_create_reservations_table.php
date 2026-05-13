42d88d28be6e8f0c5b23e896e37312d8bd3c7723
cesar jan karlo <leossuarez103@gmail.com>
2026-05-13 08:59:16 -0600

    se agregaron migracion correctas

---
+++ Backend/database/migrations/2026_05_06_230037_create_reservations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ticket_id')->constrained();
+            $table->string('userauth_id');
+            $table->integer('guest_count');
+            $table->string('status')->default('active'); // active, cancelled, completed
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
