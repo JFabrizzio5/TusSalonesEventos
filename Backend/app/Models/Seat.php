7027679f9cc9e3dc1446e7c8149f899ec1c6ea53
cesar jan karlo <leossuarez103@gmail.com>
2026-05-14 12:41:16 -0600

    se agregarom protectes faltantes

--- Backend/app/Models/Seat.php
+++ Backend/app/Models/Seat.php
@@ -7,7 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Seat extends Model
 {
     //Proteccion contra asignacion masiva
-
+    protected $fillable = [
+        'zone_id',
+        'row',
+        'number'
+    ];
+    //Casting de dates
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
     //Realcion muchos a uno (zonas-asientos)
     public function zone()
     {
