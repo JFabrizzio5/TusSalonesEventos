4ad5e1983bc31153ac3d6146a1e4ebaaaa84eac6
cesar jan karlo <leossuarez103@gmail.com>
2026-05-15 11:13:47 -0600

    se agregaron los props y funciones de relaciones

--- Backend/app/Models/Ticket.php
+++ Backend/app/Models/Ticket.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    //Protected filled
+    protected $fillable = [
+        'event_id',
+        'seat_id',
+        'price'
+    ];
+
+    //Casting de tipos de datos
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+    
+    //Relaciónes con un evento a un asiento
+    public function event()
+    {
+        return $this->belongsTo(Event::class);
+    }
+
+    //Relaciónes con un asiento a un ticket
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
 }
