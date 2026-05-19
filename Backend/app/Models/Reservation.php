b166569ce6651d5eec52ea2c931d59d16505376c
cesar jan karlo <leossuarez103@gmail.com>
2026-05-19 09:40:05 -0600

    agregaron las relaciones con las tablas

--- Backend/app/Models/Reservation.php
+++ Backend/app/Models/Reservation.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+        'ticket_id',
+        'userauth_id',
+        'guest_count',
+        'status'
+    ];
+
+    protected $casts = [
+        'guest_count' => 'integer',
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+    /**
+     * Relación:
+     * Una reservación pertenece a un ticket
+     */
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
 }
