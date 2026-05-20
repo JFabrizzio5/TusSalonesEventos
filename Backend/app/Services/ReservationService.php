d865edf3eb3f552f43286dfa5ea294fa60069925
cesar jan karlo <leossuarez103@gmail.com>
2026-05-20 10:40:38 -0600

    se agrego la validate a store

--- Backend/app/Services/ReservationService.php
+++ Backend/app/Services/ReservationService.php
@@ -4,10 +4,18 @@ namespace App\Services;
 
 use App\Models\Reservation;
 use App\Models\Ticket;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
+use App\validators\ReservationValidator;
 
 class ReservationService
 {
+    protected ReservationValidator $validator;
+
+    public function __construct(ReservationValidator $validator)
+    {
+        $this->validator = $validator;
+    }
+
     /**
      * Obtener todas las reservaciones
      */
@@ -29,34 +37,19 @@ class ReservationService
      */
     public function store(array $data)
     {
-        // Verificar que exista el ticket
-        $ticket = Ticket::with('seat')->findOrFail($data['ticket_id']);
-        $seat = $ticket->seat;
+        return DB::transaction(function () use ($data){
+            // Verificar el ticket
+            $ticket = Ticket::with('seat')->findOrFail($data['ticket_id']);
+            $this->validator->validateTicketAvailability($ticket);
+            
+            //Cambiar estado del asiento a reservado
+            $ticket->seat->update([
+                'status' => 'reserved'
+            ]);
 
-        // Verificar que el estado del asiento
-        if($seat->status === 'reserved'){
-            throw new \Exception('El asiento ya está reservado');
-        }
-        if($seat->status === 'sold'){
-            throw new \Exception('El asiento ya está vendido');
-        }
-
-        // Verificar si ya existe reservación activa
-        $reservationExists = Reservation::where('ticket_id', $ticket->id)
-            ->where('status', 'active')
-            ->exists();
-
-        if ($reservationExists) {
-            throw new \Exception('El ticket ya tiene una reservación activa');
-        }
-
-        //Cambiar estado del asiento a reservado
-        $seat->update([
-            'status' => 'reserved'
-        ]);
-
-        //crear reservacion
-        return Reservation::create($data);
+            //crear reservacion
+            return Reservation::create($data);
+        });
     }
 
     /**
@@ -76,25 +69,27 @@ class ReservationService
      */
     public function updateStatus(int $id, string $status)
     {
-        $reservation = Reservation::findOrFail($id);
+        return DB::transaction(function () use ($id, $status){
+            $reservation = Reservation::findOrFail($id);
 
-        $seat=$reservation->ticket->seat;
+            $seat=$reservation->ticket->seat;
 
-        if($status === 'cancelled') {
-            $seat->update([
-                'status' => 'available'
+            if($status === 'cancelled') {
+                $seat->update([
+                    'status' => 'available'
+                ]);
+            }
+            if($status === 'completed') {
+                $seat->update([
+                    'status' => 'sold'
+                ]);
+            }
+
+            $reservation->update([
+                'status' => $status
             ]);
-        }
-        if($status === 'completed') {
-            $seat->update([
-                'status' => 'sold'
-            ]);
-        }
-
-        $reservation->update([
-            'status' => $status
-        ]);
-        return $reservation;
+            return $reservation;
+        });
     }
 
     /**
@@ -102,14 +97,16 @@ class ReservationService
      */
     public function delete(int $id)
     {
-        $reservation = Reservation::findOrFail($id);
+        return DB::transaction(function () use ($id){
+            $reservation = Reservation::findOrFail($id);
         
-        $reservation->ticket->seat->update([
-                'status' => 'available'
-            ]);
-        
-        $reservation->delete();
+            $reservation->ticket->seat->update([
+                    'status' => 'available'
+                ]);
+            
+            $reservation->delete();
 
-        return true;
+            return true;
+        });
     }
 }
