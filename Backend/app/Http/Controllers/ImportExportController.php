24b6a140761f55e9026845b08b28b78659529950
cesar jan karlo <leossuarez103@gmail.com>
2026-05-21 11:06:00 -0600

    se agrego el service de exportImportReserv y el  ImportExport/ExportReservRequest

--- Backend/app/Http/Controllers/ImportExportController.php
+++ Backend/app/Http/Controllers/ImportExportController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Requests\ImportExport\ImportRequest;
 use App\Http\Requests\ImportExport\ExportRequest;
+use App\Http\Requests\ImportExport\ExportReservRequest;
 use App\Services\ImportExportService;
+use App\Services\ImportExportReservService;
 use App\Utils\ApiResponse;
 
 class ImportExportController extends Controller
 {
     use ApiResponse;
 
-    public function __construct(protected ImportExportService $importExportService) 
+    public function __construct(protected ImportExportService $importExportService, protected ImportExportReservService $importExportReservationService) 
     {
     }
-
+    
     /**
      * Importar CSV
      */
@@ -45,6 +47,46 @@ class ImportExportController extends Controller
             $this->importExportService->exportToCSV($salonId,$filePath);
             return response()->download($filePath)->deleteFileAfterSend(true);
         }catch(ModelNotFoundException $e){
+            return $this->notFound();
+        }
+    }
+    /**
+     * Importar reservaciones
+     */
+    public function importReservations(ImportRequest $request)
+    {
+        try {
+            $filePath = $request
+                ->file('file')
+                ->getRealPath();
+
+            $this->importExportReservationService->importReservationsFromCSV(($filePath));
+            return $this->success(null,'Reservaciones importadas correctamente');
+
+        } catch (ModelNotFoundException $e) {
+            return $this->notFound();
+        }
+    }
+
+    /**
+     * Exportar reservaciones
+     */
+    public function exportReservations(ExportReservRequest $request)
+    {
+        try {
+            $eventId = $request->event_id;
+
+            $directory = storage_path('app/exports');
+
+            if (!file_exists($directory)) {
+                mkdir($directory, 0777, true);
+            }
+
+            $filePath = $directory .
+                "/reservations_event_{$eventId}.csv";
+            $this->importExportReservationService->exportReservationsFromCSV($eventId,$filePath);
+            return response()->download($filePath)->deleteFileAfterSend(true);
+        } catch (ModelNotFoundException $e) {
             return $this->notFound();
         }
     }
